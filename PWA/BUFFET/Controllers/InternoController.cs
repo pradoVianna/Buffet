08e1d33051c1f64fa2ec3eacd719cf48b199f335
pradopedro <pedroprado.index@gmail.com>
2021-03-28 13:19:55 -0300

    Finalização da atividade

---
+++ PWA/BUFFET/Controllers/InternoController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BUFFET.Controllers
+{
+    public class InternoController : Controller
+    {
+        // GET
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult TermosUso()
+        {
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult Ajuda()
+        {
+            return View();
+        }
+        public IActionResult Agenda()
+        {
+            return View();
+        }
+        public IActionResult Eventos()
+        {
+            return View();
+        }
+        public IActionResult Financeiro()
+        {
+            return View();
+        }
+        public IActionResult MinhaConta()
+        {
+            return View();
+        }
+    }
+}
