564913bb42551d7597718fb0056e58c6e8ec0cd9
Cesar <cesar.leitejm@hotmail.com>
2021-05-18 21:00:25 -0300

    Cotinuação

---
+++ PWA/BUFFET/Controllers/ControllerLocais.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BUFFET.Controllers
+{
+    public class ControllerLocais : Controller
+    {
+        // GET
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
